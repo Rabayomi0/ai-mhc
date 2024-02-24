3507ee4fcc05374c3aebf89bac1f58252bfc6600
Rabayomi0 <riabayomi77@gmail.com>
2024-02-23 20:46:24 -0500

    first commit

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div>
+      <h1>A.I. Mental Health Coach</h1>
+      <p>What kind of personality would you like me to have?</p>
+      <input size={50}></input>
+    </div>
+  );
+}
+
+export default Landing;
